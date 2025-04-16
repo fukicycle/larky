85387bd2da1710409069f5359389212b470d2aad
fukicycle <lego.sato.4135@gmail.com>
2025-04-16 13:58:27 +0900

    Add repositories

--- View/Program.cs
+++ View/Program.cs
@@ -1,3 +1,7 @@
+using Domain.Enums;
+using Domain.Interfaces;
+using Infrastructure.DTO;
+using Infrastructure.Firebase;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using View;
@@ -7,5 +11,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddKeyedSingleton(typeof(IRepository<UserDTO>), nameof(FirebaseScheme.Users), new FirebaseRepository<UserDTO>(FirebaseScheme.Users));
+builder.Services.AddKeyedSingleton(typeof(IRepository<WordDTO>), nameof(FirebaseScheme.Words), new FirebaseRepository<WordDTO>(FirebaseScheme.Words));
+builder.Services.AddKeyedSingleton(typeof(IRepository<RankingDTO>), nameof(FirebaseScheme.Rankings), new FirebaseRepository<RankingDTO>(FirebaseScheme.Rankings));
+builder.Services.AddKeyedSingleton(typeof(IRepository<BadgeDTO>), nameof(FirebaseScheme.Badges), new FirebaseRepository<BadgeDTO>(FirebaseScheme.Badges));
 
 await builder.Build().RunAsync();
