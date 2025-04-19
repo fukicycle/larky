af71d36d14a7da9e9a6a4bb230dff60cf91aad5d
fukicycle <lego.sato.4135@gmail.com>
2025-04-19 23:53:49 +0900

    シンセサイザーを追加

--- View/Program.cs
+++ View/Program.cs
@@ -4,6 +4,7 @@ using Infrastructure.DTO;
 using Infrastructure.Firebase;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 using View;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,6 +12,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSpeechSynthesis();
 builder.Services.AddKeyedSingleton(typeof(IRepository<UserDTO>), nameof(FirebaseScheme.Users), new FirebaseRepository<UserDTO>(FirebaseScheme.Users));
 builder.Services.AddKeyedSingleton(typeof(IRepository<WordDTO>), nameof(FirebaseScheme.Words), new FirebaseRepository<WordDTO>(FirebaseScheme.Words));
 builder.Services.AddKeyedSingleton(typeof(IRepository<RankingDTO>), nameof(FirebaseScheme.Rankings), new FirebaseRepository<RankingDTO>(FirebaseScheme.Rankings));
