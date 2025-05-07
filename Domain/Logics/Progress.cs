13cfacf0719fb17ac1e88b6675d1ad107ea3c4d0
fukicycle <lego.sato.4135@gmail.com>
2025-05-07 17:35:02 +0900

    ロジック修正

--- Domain/Logics/Progress.cs
+++ Domain/Logics/Progress.cs
@@ -2,14 +2,13 @@ namespace Domain.Logics;
 
 public static class Progress
 {
-    public static double CalculateValue(int? current, int? total)
+    public static int CalculateValue(int? current, int? total)
     {
-        if (current == null || total == null)
+        if (current == null || total == null || total == 0)
         {
             return 0;
         }
-        var currentDouble = Convert.ToDouble(current);
-        var totalDouble = Convert.ToDouble(total);
-        return Math.Round(currentDouble / totalDouble * 100.0);
+        double percent = (double)current.Value / total.Value * 100;
+        return (int)Math.Round(percent);
     }
 }
