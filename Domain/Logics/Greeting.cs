fa983902403969040abe78659ee1f4e91769f047
fukicycle <lego.sato.4135@gmail.com>
2025-05-07 17:28:16 +0900

    挨拶のロジック変更

--- Domain/Logics/Greeting.cs
+++ Domain/Logics/Greeting.cs
@@ -2,18 +2,17 @@ namespace Domain.Logics;
 
 public static class Greeting
 {
-    public static string GetGreeting()
+    public static string GetGreeting(TimeSpan? time = null)
     {
-        var currentTime = DateTime.Now.TimeOfDay;
-        if (currentTime >= TimeSpan.FromHours(2) && currentTime <= TimeSpan.FromHours(10))
+        var currentTime = time ?? DateTime.Now.TimeOfDay;
+        if (currentTime >= TimeSpan.FromHours(5) && currentTime < TimeSpan.FromHours(10)) // 5:00 ~ 9:59
         {
             return "おはよう";
         }
-
-        if (currentTime > TimeSpan.FromHours(10) && currentTime <= TimeSpan.FromHours(18))
+        if (currentTime >= TimeSpan.FromHours(10) && currentTime < TimeSpan.FromHours(18)) // 10:00 ~ 17:59
         {
             return "こんにちは";
         }
-        return "こんばんは";
+        return "こんばんは"; // 深夜〜早朝を含む
     }
 }
